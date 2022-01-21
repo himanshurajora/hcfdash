9a0e62ee0a0adbfab8ec6b6e15d83a1bfb3ed5c2
himanshurajora <iamahalofan@gmail.com>
2022-01-21 22:02:33 +0530

    dash links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import toast, { Toaster } from 'react-hot-toast'
 import Container from '../components/Container/Container'
 export default function Home() {
@@ -8,7 +9,44 @@ export default function Home() {
     <>
       <Container>
         <div className="title">
-          Business Statics Will Be Here
+          DashBoard
+        </div>
+        <div className="title is-4">
+          For Herbs
+        </div>
+        <div className="columns">
+          <div className="column is-7">
+            <div className="columns">
+              <div className="column">
+                <Link href="/herbs/add">
+                  <a className="button is-primary">
+                    Add Herb
+                  </a>
+                </Link>
+              </div>
+              <div className="column">
+                <Link href="/herbs/history">
+                  <a className="button is-primary">
+                    Herb History
+                  </a>
+                </Link>
+              </div>
+              <div className="column">
+                <Link href="/herbs/view">
+                  <a href="" className="button is-primary">
+                    List of Herbs
+                  </a>
+                </Link>
+              </div>
+              <div className="column">
+                <Link href="/herbs/lowlevel">
+                  <a href="" className="button is-primary">
+                    Below Low Level
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </>
